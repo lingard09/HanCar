89f920bad288bef39a1bf01a33289ed1aaca3c10
lingardd09 <sherlockicker@gmail.com>
2025-11-24 15:46:19 +0900

    style: remove useless line

---
+++ hancar/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  hancar
+//
+//  Created by kinnwonjin on 11/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hi")
+    }
+}
+
